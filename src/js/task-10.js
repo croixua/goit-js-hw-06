46b0eaf8a54949ad0649d710dd79e877377d81bb
croixua <croixua@gmail.com>
2022-01-13 00:55:05 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,24 @@
+const boxesRef = document.querySelector('#boxes');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+
+createBtn.addEventListener('click', createBoxes);
+
+function createBoxes(amount) {
+  let width = '30px';
+  let height = '30px';
+  const color = getRandomHexColor();
+  const box = document.createElement('div');
+
+  box.style.width = width;
+  box.style.height = height;
+  box.style.backgroundColor = color;
+
+  boxesRef.append(box);
+
+  const array = [];
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }

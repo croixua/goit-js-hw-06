25abbbcd99efa67cafdfeb729ed29c4226756807
croixua <croixua@gmail.com>
2022-01-12 23:54:32 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+const changeColorBtnRef = document.querySelector('.change-color');
+const colorTextValueRef = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+changeColorBtnRef.addEventListener('click', changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+  bodyRef.style.backgroundColor = `${color}`;
+  colorTextValueRef.textContent = `${color}`;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }

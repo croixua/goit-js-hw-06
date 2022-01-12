2eaa88c05710993228351786eb5788a48d547fc8
croixua <croixua@gmail.com>
2022-01-13 00:53:17 +0200

    refactoring task-04

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,26 +1,25 @@
-const btnDecrement = document.querySelector('[data-action="decrement"]')
-const btnIncrement = document.querySelector('[data-action="increment"]')
-const btnReset = document.querySelector('.reset-btn')
-const value = document.querySelector('#value')
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const btnIncrement = document.querySelector('[data-action="increment"]');
+const btnReset = document.querySelector('.reset-btn');
+const value = document.querySelector('#value');
 
 let counterValue = 0;
 
-btnDecrement.addEventListener('click', decrementValue)
-btnIncrement.addEventListener('click', incrementValue)
-btnReset.addEventListener('click', resetValue)
+btnDecrement.addEventListener('click', decrementValue);
+btnIncrement.addEventListener('click', incrementValue);
+btnReset.addEventListener('click', resetValue);
 
 function decrementValue() {
-    counterValue -= 1
-    return value.textContent = counterValue
+  counterValue -= 1;
+  return (value.textContent = counterValue);
 }
 
 function incrementValue() {
-    counterValue += 1
-    return value.textContent = counterValue
+  counterValue += 1;
+  return (value.textContent = counterValue);
 }
 
 function resetValue() {
-    counterValue = 0
-    return value.textContent = counterValue
+  counterValue = 0;
+  return (value.textContent = counterValue);
 }
-

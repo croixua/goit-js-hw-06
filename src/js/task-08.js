0e834cc8799d0b926a5c48f13a050e2f0e38471a
croixua <croixua@gmail.com>
2022-01-12 23:28:04 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  const formDataEl = {};
+
+  if (email.value === '' || password.value === '') {
+    return alert('Аллё, господин, заполни поля!');
+  }
+
+  formDataEl.email = email.value;
+  formDataEl.password = password.value;
+
+  console.log(formDataEl);
+
+  return formRef.reset();
+}

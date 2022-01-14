46651b87c3003b52c3d17ca01e4cbda21f89a57f
croixua <croixua@gmail.com>
2022-01-14 17:50:35 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,12 @@
 const fontSizeСontrolRef = document.querySelector('#font-size-control');
 const textRef = document.querySelector('#text');
 
+textRef.style.fontSize = `${fontSizeСontrolRef.value}px`;
 fontSizeСontrolRef.addEventListener('input', sizeText);
 
 function sizeText(event) {
   const currentFontSize = event.currentTarget.value;
+  console.log(currentFontSize);
 
   textRef.style.fontSize = `${currentFontSize}px`;
   textRef.style.transition = '250ms linear';
